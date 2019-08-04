e7e34b5250f3977e6148b1c3761911b8e8dfd875
cw2981 <cw2981>
2019-08-04 04:37:45 +0000

    image blur #3 completed. major changes, manhattan distance, and <= blur distance.

--- pixels.rb
+++ pixels.rb
@@ -16,22 +16,24 @@ class Image
     ones
   end
 
-  def blur!
+  def blur!(blur_distance)
     ones = grab_ones
-      @picture.each_with_index do |row, row_number|
-        row.each_with_index do |pixel, col_number|
-          ones.each do |found_row_number, found_col_number|
 
-            if row_number == found_row_number && col_number == found_col_number
-              @picture[row_number -1][col_number] = 1 unless row_number == 0 
-              @picture[row_number +1][col_number] = 1 unless row_number >= 3 
-              @picture[row_number][col_number -1] = 1 unless col_number == 0 
-              @picture[row_number][col_number +1] = 1 unless col_number >= 3
-            end
+    @picture.each_with_index do |row, row_number|
+      row.each_with_index do |pixel, col_number|
+        ones.each do |found_row_number, found_col_number|
+          if manhattan_distance(col_number, row_number, found_col_number, found_row_number) <= blur_distance
+            @picture[row_number][col_number] = 1
           end
         end
       end
-     output_image
+    end
+  end
+
+  def manhattan_distance (x1, y1, x2, y2)
+    horizontal_distance = (x2 - x1).abs
+    vertical_distance = (y2 - y1).abs
+    horizontal_distance + vertical_distance
   end
 
   def output_image
@@ -42,13 +44,19 @@ class Image
 end
 
 image = Image.new([
-[0, 0, 0, 0],
-[0, 1, 0, 0],
-[0, 0, 0, 1],
-[0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 1, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 1]
 ])
 
-
+# original image
 image.output_image
 puts
-image.blur!
+
+# blurred image
+image.blur!(2)
+image.output_image
